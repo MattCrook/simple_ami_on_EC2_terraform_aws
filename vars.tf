c5dfedb05f56f930f1e76def5d2ae41236eebf38
Matt Crook <matt.crook@getbuilt.com>
2021-04-15 18:33:52 -0500

    fix: fix variable comments and description.

--- vars.tf
+++ vars.tf
@@ -2,9 +2,9 @@
 // and run apply, TF will interactivly prompt you to enter a value for server_port
 // and show the description of the var.
 
-// terraform plan -var "server_port=8080"
-// or
-// export TF_VAR_server_port=8080
+// terraform plan -var "server_port=8080" or export TF_VAR_server_port=8080
+// However providing the default means you will not have to pass the var in when running terraform plan.
+
 variable "server_port" {
     description = "The port the server will use for HTTP requests"
     type        = number
